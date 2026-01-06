46f6b3cb6a82f7b2dba1774ea33133a30f76bec7
gabrielg671936-maker <gabrielg671936@gmail.com>
2026-01-06 09:22:16 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log ("js console");
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+        let games = JSON.parse(xhttp.responseText);
+        console.log(game[0].title);
+    }
+};
+
+xhttp.open("GET","list.json",true);
+xhttp.send();
